9395b19440bbcdf330e26e8120bab1aa7b3413e8
KN2018 <5693386765@qq.com>
2019-03-21 08:02:56 +0800

    将日志信息写入文件

--- Log4NetDemo/Program.cs
+++ Log4NetDemo/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using log4net;
+using log4net.Config;
 namespace Log4NetDemo
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            //创建一个日志对象，返回的对象类实现了ILog接口中的所有方法
+            ILog Log = LogManager.GetLogger("log4NetDemo");
+            //应用应用程序配置文件中的log4net配置 
+            log4net.Config.XmlConfigurator.Configure();
+            Log.Info("我是Info");
+            Log.Debug("我是Debug");
+            Log.Error("我是Error");
+
         }
     }
 }
