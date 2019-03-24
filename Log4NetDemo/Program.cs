d4751ad57948dfb5fb8099e117eda2f476a79745
KN2018 <5693386765@qq.com>
2019-03-24 15:02:05 +0800

    将日志文件写入数据库

--- Log4NetDemo/Program.cs
+++ Log4NetDemo/Program.cs
@@ -15,10 +15,16 @@ namespace Log4NetDemo
             //创建一个日志对象，返回的对象类实现了ILog接口中的所有方法
             ILog Log = LogManager.GetLogger("log4NetDemo");
             //应用应用程序配置文件中的log4net配置 
-            log4net.Config.XmlConfigurator.Configure();
+            XmlConfigurator.Configure();
+            //LogicalThreadContext.Properties["Operator"] 属性:获取或设置Log4Net关于“日志输出到数据库”配置中名为"Operator"的参数值
+            LogicalThreadContext.Properties["Operator"] = "李明";
+            //输出Info级别的日志信息
             Log.Info("我是Info");
+            //输出Debug级别的日志信息
             Log.Debug("我是Debug");
+            //输出Error级别的日志信息
             Log.Error("我是Error");
+            
             Console.ReadLine();
         }
     }
